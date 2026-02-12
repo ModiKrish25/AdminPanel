8e67bed28c7e13ae2f2c27c9fae4deb3cd4793da
ModiKrish25 <krish.h.2506modi@gmail.com>
2026-02-12 11:26:56 +0530

    Vercel

--- AdminPanel/adminpanel/src/api/apiConfig.js
+++ AdminPanel/adminpanel/src/api/apiConfig.js
@@ -1,2 +1,2 @@
 const isProduction = import.meta.env.PROD;
-export const API_BASE_URL = isProduction ? '/api' : 'http://127.0.0.1:5000';
+export const API_BASE_URL = isProduction ? 'https://adminpanel1-i9v3.onrender.com/api' : 'http://127.0.0.1:5000';
